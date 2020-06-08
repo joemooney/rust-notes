708f4077454adee4da3d72d77047b841cbecdb17
Joe Mooney <joe.mooney@gmail.com>
2020-06-07 21:31:14 -0700

    updated publish.sh for rsync

--- publish.sh
+++ publish.sh
@@ -2,10 +2,11 @@
 
 url=$(git config --get remote.origin.url | sed 's,git@github.com:,,;s,/,.github.io/,;s,^,https://,;s,.git$,/,')
 
-mdbook build                         && \
-git status                           && \
-echo 'git commit -am'                && \
-echo -n "Comment: " && read comment  && \
-git commit -am "$comment"            && \
-git push                             && \
+mdbook build                                          && \
+rsync -avx --delete --info=progress2 ./book/ ./docs/  &&\
+git status                                            && \
+echo 'git commit -am'                                 && \
+echo -n "Comment: " && read comment                   && \
+git commit -am "$comment"                             && \
+git push                                              && \
 echo "Published: $url"
