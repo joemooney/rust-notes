7fc4379fd6d4564aaddde99193f2b8fc6d41b8fb
Joe Mooney <joe.mooney@gmail.com>
2020-06-07 21:26:19 -0700

    publish script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+url=$(git config --get remote.origin.url | sed 's,git@github.com:,https://,;s,.git$,/,')
+
+mdbook build                         && \
+git status                           && \
+echo 'git commit -am'                && \
+echo -n "Comment: " && read comment  && \
+git commit -am "$comment"            && \
+git push                             && \
+echo "Published: $url"
