83428253f05ab2154f2e36d5de44a2a936bfa342
Joe Mooney <joe.mooney@gmail.com>
2020-06-14 14:39:57 -0700

    updates to readme

---
+++ git_functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git_no_updates() { x=$(git status | grep 'nothing added to commit but untracked files present' | wc -l); [ $x -ne 0 ]; return $?; }
+git_untracked() { x=$(git status | grep 'Untracked files' | wc -l); [ $x -gt 0 ]; return $?; }
+
+git_check_untracked() {
+    git_untracked || { echo "[info] No untracked files"; return 0; }
+    [ $opt_force -eq 1 ] && echo "[warn] You have untracked files" && return 0
+    echo "[error] You have untracked files, use --force" && exit 1 
+}
+
